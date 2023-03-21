1ec6876285dc2369c4ef55188ca0e776c7956a8b
biswascom <abjessbd@gmail.com>
2023-03-21 20:37:20 +0600

    First React App with Api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <Display></Display>
     </div>
   );
 }
+
+function Display(){
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+
+  return (
+    <div>
+      <h1>Display Api Data with React</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email} address={user?.address?.city}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return(
+    <div className='user'>
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+      <p>City: {props.address}</p>
+    </div>
+  )
+}
+
+// function Counter(){
+//   const [count, setCount] = useState(0);
+//   const increaseCounter = () =>setCount(count + 1);
+//   const decreaseCounter = () => setCount(count - 1);
+
+//   // const IncreaseCounter = () =>{
+//   //   const newCount = count + 1;
+//   //   setCount(newCount);
+//   // }
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={increaseCounter}>Increase</button>
+//       <button onClick={decreaseCounter}>Decrease</button>
+//     </div>
+//   )
+// }
 
 export default App;
